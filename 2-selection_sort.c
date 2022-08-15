3e88440b442a09f74f961da29aab966cb6ddb706
Samuel <samueldesta323@gmail.com>
2022-08-16 00:31:49 +0300

    modified the selction algo

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,21 +9,23 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j;
+	int *low, tmp;
 
-	if (array == NULL)
+	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
+		low = array + i;
 		for (j = i + 1; j < size; j++)
+			low = (array[j] < *low) ? (array + j) : low;
+
+		if ((array + i) != low)
 		{
-			if (array[j] < array[i])
-			{
-				array[j] = array[j] + array[i];
-				array[i] = array[j] - array[i];
-				array[j] = array[j] - array[i];
-				print_array(array, size);
-			}
+			tmp = *low;
+			*low = *(array + i);
+			*(array + i) = tmp;
+			print_array(array, size);
 		}
 	}
 }
